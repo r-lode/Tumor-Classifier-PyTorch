38d046dfad2da79c5bf496368205f04f04947c6f
strongshell <rlode24@gmail.com>
2023-10-22 17:46:39 -0500

    Updating docstring

--- src/CNN.py
+++ src/CNN.py
@@ -4,4 +4,10 @@ from torch import nn
 class Custom_TinyVGG(nn.Module):
     '''
     Neural network based on the TinyVGG architecture
+
+    Args:
+
+    input_shape (int): Number of color channels
+    hidden_units (int): Number of hidden neurons in network layers
+    output_shape (int): Number of tumor classes 
     '''
