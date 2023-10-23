8f41332184703ccc5f5ce0998e1cc10f43df63f1
strongshell <rlode24@gmail.com>
2023-10-23 13:27:22 -0500

    Updating CNN.py, filling out neural network class

--- src/CNN.py
+++ src/CNN.py
@@ -8,6 +8,59 @@ class Custom_TinyVGG(nn.Module):
     Args:
 
     input_shape (int): Number of color channels
-    hidden_units (int): Number of hidden neurons in network layers
+    num_hidden_neurons (int): Number of hidden neurons in network layers
     output_shape (int): Number of tumor classes 
+
+    Attributes:
+        conv_block_1 (nn.Sequential): First convolutonal block, contains two
+        layers and max pooling
+        conv_block_2 (nn.Sequential): Second convolutional block, contains two
+        layers and max pooling
+        classifier (nn.Sequential): classifier layer
+
+    Example Usage:
+
+    model = Custom_TinyVGG(input_shape = 3, #RBG color channels
+            num_hidden_neurons = 10, 
+            output_shape = 4) #There are four tumor classes
     '''
+    def __init__(self, input_shape, num_hidden_neurons, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=num_hidden_neurons, 
+                      kernel_size=3, 
+                      stride=1, 
+                      padding=1), 
+            nn.ReLU(),
+            nn.Conv2d(in_channels=num_hidden_neurons, 
+                      out_channels=num_hidden_neurons,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(num_hidden_neurons, 
+                      num_hidden_neurons, 
+                      kernel_size=3, 
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(num_hidden_neurons, 
+                      num_hidden_neurons, 
+                      kernel_size=3, 
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=num_hidden_neurons*56*56,
+                      out_features=output_shape)
+        )
+
+    def forward(self, x: torch.Tensor):
+
+        return self.classifier(self.conv_block_2(self.conv_block_1(x))) 
