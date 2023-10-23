1ad2dd9088645d4b8902f206a6436a6cecf02271
strongshell <rlode24@gmail.com>
2023-10-23 15:02:12 -0500

    Adding model test function

--- src/train_test.py
+++ src/train_test.py
@@ -44,7 +44,55 @@ def train_model(input_model: torch.nn.Module,
         #update train accuracy
         train_accuracy += (predicted_tumor_class == y).sum().item()/len(y_predictions)
 
-        return train_loss, train_accuracy
+    adjusted_train_loss = train_loss / len(dataloader)
+    adjusted_accuracy = train_accuracy / len(dataloader)
+    return adjusted_train_loss, adjusted_accuracy
+
+
+def test_model(test_model: torch.nn.Module,
+               dataloader: torch.utils.DataLoader,
+               loss_fn: torch.nn.Module,
+               device):
+    
+    #start evaluation mode
+    test_model.eval()
+
+    sum_test_loss = 0
+    test_accuracy = 0
+
+    #start inference mode
+
+    with torch.inference_mode():
+
+        for batch, (X, y) in enumerate(dataloader):
+
+            X = X.to(device)
+            y = y.to(device)
+
+            #forward pass
+            test_logits = test_model(X)
+
+            #loss
+            loss = loss_fn(test_logits, y)
+            sum_test_loss += loss.item()
+
+            #calculate predicted test tumor label
+            tumor_pred_labels = test_logits.argmax(dim = 1)
+            test_accuracy += ((tumor_pred_labels == y).sum().item()/len(tumor_pred_labels))
+
+
+
+    adjusted_test_loss = sum_test_loss / len(dataloader)
+    adjusted_test_accuracy = test_accuracy / len(dataloader)
+
+    return adjusted_test_loss, adjusted_test_accuracy
+ 
+
+
+
+
+
+
 
 
 
